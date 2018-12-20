0027dfacc410c9eaa18e95928c04e1ac7c54b531
Erik Zetterqvist <erik.zetterquist@trafikverket.se>
2018-12-20 05:21:20 +0100

    HostBuilderExtensions

---
+++ Topshelf.Extensions.Hosting/HostBuilderExtensions.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace Topshelf.Extensions.Hosting
+{
+    public static class HostBuilderExtensions
+    {
+        public static IHostBuilder UseTopshelfLifetime(this IHostBuilder hostBuilder)
+        {
+            return hostBuilder.ConfigureServices((hostContext, services) =>
+            {
+                services.AddSingleton<IHostLifetime, TopshelfLifetime>();
+            });
+        }
+
+        /// <summary>
+        /// Builds and run the host as a Topshelf service.
+        /// </summary>
+        /// <param name="hostBuilder"></param>
+        /// <param name="options">Configuration of the topshelf service.</param>
+        /// <returns></returns>
+        public static TopshelfExitCode RunAsTopshelfService(this IHostBuilder hostBuilder, Action<HostConfigurators.HostConfigurator> configureTopshelfHost)
+        {
+            if (configureTopshelfHost == null)
+                throw new ArgumentNullException(nameof(configureTopshelfHost));
+
+            hostBuilder.UseTopshelfLifetime();
+
+            var rc = HostFactory.Run(x =>
+            {
+                configureTopshelfHost(x);
+                x.Service<IHost>((Action<Topshelf.ServiceConfigurators.ServiceConfigurator<IHost>>)(s =>
+                {
+                    s.ConstructUsing(() => hostBuilder.Build());
+                    s.WhenStarted(service =>
+                    {
+                        service.Start();
+                    });
+                    s.WhenStopped(service =>
+                    {
+                        service.StopAsync();
+                    });
+                }));
+            });
+
+            return rc;
+        }
+    }
+}
---
+++ Topshelf.Extensions.Hosting/TopshelfLifetime.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Topshelf.Extensions.Hosting
+{
+    public class TopshelfLifetime : IHostLifetime
+    {
+        //private readonly TaskCompletionSource<object> _delayStart = new TaskCompletionSource<object>();
+
+        public TopshelfLifetime(IApplicationLifetime applicationLifetime, IServiceProvider services)
+        {
+            ApplicationLifetime = applicationLifetime ?? throw new ArgumentNullException(nameof(applicationLifetime));
+        }
+
+        private IApplicationLifetime ApplicationLifetime { get; }
+
+        public Task WaitForStartAsync(CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
