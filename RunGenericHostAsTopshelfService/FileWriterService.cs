7d2f5291e766f1627112ade5cc9ef9a8c35571d1
Erik Zetterqvist <erik.zetterquist@trafikverket.se>
2018-12-20 05:37:28 +0100

    added sample

---
+++ RunGenericHostAsTopshelfService/FileWriterService.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RunGenericHostAsTopshelfService
+{
+    class FileWriterService : IHostedService, IDisposable
+    {
+        private const string Path = @"c:\temp\TestApplication.txt";
+
+        private Timer _timer;
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            if (cancellationToken.IsCancellationRequested) return Task.FromCanceled(cancellationToken);
+
+            _timer = new Timer(
+                (e) => WriteTimeToFile(),
+                null,
+                TimeSpan.Zero,
+                TimeSpan.FromSeconds(10));
+
+            return Task.CompletedTask;
+        }
+
+        public void WriteTimeToFile()
+        {
+            if (!File.Exists(Path))
+            {
+                using (var sw = File.CreateText(Path))
+                {
+                    sw.WriteLine(DateTime.UtcNow.ToString("O"));
+                }
+            }
+            else
+            {
+                using (var sw = File.AppendText(Path))
+                {
+                    sw.WriteLine(DateTime.UtcNow.ToString("O"));
+                }
+            }
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            _timer?.Change(Timeout.Infinite, 0);
+
+            return Task.CompletedTask;
+        }
+
+        public void Dispose()
+        {
+            _timer?.Dispose();
+        }
+    }
+}
---
+++ RunGenericHostAsTopshelfService/Program.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using Topshelf.Extensions.Hosting;
+
+namespace RunGenericHostAsTopshelfService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new HostBuilder()
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddHostedService<FileWriterService>();
+                });
+
+            builder.RunAsTopshelfService(hc =>
+            {
+                hc.SetServiceName("GenericHostInTopshelf");
+                hc.SetDisplayName("Generic Host In Topshelf");
+                hc.SetDescription("Runs a generic host as a Topshelf service.");
+            });
+        }
+    }
+}
